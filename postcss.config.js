b777e467418d7c33287eb99d8db5f7a29182d768
areve <areve@challen.info>
2021-02-20 21:31:57 +0000

    tidy

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,9 @@
 module.exports = {
   plugins: {
     'postcss-mixins': {},
-    //require('postcss-sassy-mixins'),
-
-    //'postcss-simple-vars': {},
-    //'postcss-conditionals': {},
     'postcss-advanced-variables': {},
     'postcss-nested': {},
-    //'postcss-at-rules-variables': {},
-    //'autoprefixer': {},
-    //'postcss-assets': {},
+    'autoprefixer': {},
+    'postcss-assets': {},
   }
 }
